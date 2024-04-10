777ca11b89302a85652e24e3808cbb79153fb9a4
Dipayan Das <105163490+dasdipayan2002@users.noreply.github.com>
2024-04-10 11:13:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	
+    // Convert both strings to lower case
+    let lowerCaseStr = s1.toLowerCase();
+    let lowerCaseSubStr = s2.toLowerCase();
+
+    // Use the indexOf method
+    let index = lowerCaseStr.indexOf(lowerCaseSubStr);
+
+    // Return the result
+    return index;
+
 }
 
 // Please do not change the code below
